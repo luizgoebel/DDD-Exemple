c0c1d671c5c9b8056d19e5c54100746cd59c9e19
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2024-03-01 11:39:17 -0300

    criando os validations

---
+++ src/DDD.Models/Validations/ItemPedidoValidator.cs
@@ -0,0 +1,13 @@
+﻿using DDD.Models.Models;
+using FluentValidation;
+
+namespace DDD.Models.Validations;
+
+public class ItemPedidoValidator : AbstractValidator<ItemPedido>
+{
+    public ItemPedidoValidator()
+    {
+        //RuleFor(item => item.Quantidade).GreaterThan(0).WithMessage("A quantidade do item deve ser maior que zero.");
+        //RuleFor(item => item.PrecoUnitario).GreaterThan(0).WithMessage("O preço unitário do item deve ser maior que zero.");
+    }
+}
---
+++ src/DDD.Models/Validations/PedidoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace DDD.Models.Validations;
+
+public class PedidoValidator : AbstractValidator<PedidoValidator>
+{
+	public PedidoValidator()
+	{
+        //RuleFor(pedido => pedido.DataPedido).NotEmpty().WithMessage("A data do pedido é obrigatória.");
+        //RuleForEach(pedido => pedido.Itens).SetValidator(new ItemPedidoValidator());
+    }
+}
